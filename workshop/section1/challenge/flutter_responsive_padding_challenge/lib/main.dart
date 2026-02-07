cf2558b739b74a8f18cf77c17191c4f7421c641c
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2026-02-07 18:38:17 +0700

    Update chellenge 2

--- workshop/section1/challenge/flutter_responsive_padding_challenge/lib/main.dart
+++ workshop/section1/challenge/flutter_responsive_padding_challenge/lib/main.dart
@@ -4,16 +4,100 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    // final screenWidth = MediaQuery.sizeOf(context).width;
+    // final isLargeScreen = screenWidth >= 600;
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Responsive Padding Challenge"),),
-        body: ResponsiveCard(),
+        body: SecondChallenge(),
+        appBar: AppBar(title: Text("Responsive Navbar Challenge")),
+      ),
+    );
+  }
+}
+
+class SecondChallenge extends StatefulWidget {
+  const SecondChallenge({super.key});
+  @override
+  State<SecondChallenge> createState() => _SecondChallengeState();
+}
+
+class _SecondChallengeState extends State<SecondChallenge> {
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text("Home")),
+    Center(child: Text("Setting")),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    final isLargeScreen = screenWidth >= 600;
+    return Scaffold(
+      body: isLargeScreen ? _bottomNav() : _leftNav(),
+    );
+  }
+
+  Scaffold _bottomNav() {
+    return Scaffold(
+      body: _pages.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Scaffold _leftNav() {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            labelType: NavigationRailLabelType.all,
+            // Shows icons and labels
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings),
+                label: Text('Setting'),
+              ),
+            ],
+            // Optional customizations
+            //backgroundColor: Colors.grey[200],
+            //leading: const Icon(Icons.menu), // Widget above the destinations
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          // Main content area
+          Expanded(child: _pages[_selectedIndex]),
+        ],
       ),
     );
   }
@@ -46,7 +130,11 @@ class ResponsiveCard extends StatelessWidget {
               color: Colors.blue,
               borderRadius: BorderRadius.circular(16),
             ),
-            child: Row(children: _contentWidgets(isLargeScreen ? "Large Screen" : "Small Screen"))
+            child: Row(
+              children: _contentWidgets(
+                isLargeScreen ? "Large Screen" : "Small Screen",
+              ),
+            ),
           );
         },
       ),
@@ -57,10 +145,7 @@ class ResponsiveCard extends StatelessWidget {
     return [
       const SizedBox(width: 16, height: 106),
       Expanded(
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 18, color: Colors.white),
-        ),
+        child: Text(text, style: TextStyle(fontSize: 18, color: Colors.white)),
       ),
     ];
   }
